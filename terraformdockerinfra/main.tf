2d52ca6faabf65cddaf2c73c4f385cce85daf793
shreyashkumarp98 <shreyashkumarp98@gmail.com>
2022-04-07 13:11:40 +0530

    terraform with output

--- terraformdockerinfra/main.tf
+++ terraformdockerinfra/main.tf
@@ -14,20 +14,28 @@ resource "docker_image" "targetserver" {
   keep_locally = false
 }
 
-resource "docker_container" "web_server" {
+resource "docker_container" "web_server2" {
   image = docker_image.targetserver.name
-  name  = "web_server"
+  name  = "web_server2"
   ports {
     internal = 5000
     external =  8082
   }
 }
 
-resource "docker_container" "db_server" {
+resource "docker_container" "db_server2" {
   image = docker_image.targetserver.name
-  name  = "db_server"
+  name  = "db_server2"
   ports {
     internal = 3306
     external = 3306
   }
+}
+
+output "ips1" {
+    value = docker_container.web_server2.ip_address
+}
+
+output "ips2" {
+    value = docker_container.db_server2.ip_address
 }
